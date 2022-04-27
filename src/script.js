4d8d15a56b40c6521c27eaf0dd1af493d8f5b3aa
Anurag Singh Naruka <Anuragsinghnaruka@gmail.com>
2022-04-28 04:18:20 +0530

    betti number calculation

--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 // // importing required packages
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { NumberKeyframeTrack } from "three";
+// const gui = new dat.GUI()
 
 var arr = [];
-// horse seashell bunny cube sphere5 sphere20
+// horse seashell bunny cube sphere5 sphere20 Tangle Torus space_station x_wing helix2 RzTorus
 function readTextFile() {
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "sphere5.gts", true);
+    rawFile.open("GET", "RzTorus.gts", true);
     rawFile.onreadystatechange = function() {
         if (rawFile.readyState === 4) {
             var allText = rawFile.responseText;
@@ -21,15 +24,30 @@ readTextFile();
 var scene, camera, renderer;
 var controls;
 var obj;
-const Scale = 30;
+var stats;
+const EdgeGeometrys = [];
+const Scale = 5;
 
-const vertices = [null];
+const vertices = [
+    [0, 0, 0]
+];
+class Vertex {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
 const points = [];
 const edges = [null];
 const faces = [];
 
 let num_vertices, num_edges, num_triangles;
-let betti1, betti2, betti3;
+let betti0 = 0,
+    betti1 = 0,
+    betti2 = 0;
+
+let markedPointRadii = { radius: 1 };
 
 var adj;
 var heightFunctionList;
@@ -54,9 +72,10 @@ function fillArrays(arr) {
         let [x, y, z] = arr[i].split(" ");
         let X = Scale * Number(x);
         let Y = Scale * Number(y);
-        let Z = Scale * (Number(z));
+        let Z = Scale * Number(z);
+        let vertex = new Vertex(X, Y, Z);
 
-        vertices.push([X, Y, Z]);
+        vertices.push(vertex);
 
         // if (Z >= 0)
         //     heightFunctionList[i] = sphereHeightFunction(X, Y);
@@ -65,7 +84,6 @@ function fillArrays(arr) {
         heightFunctionList[i] = Z;
         // heightFunctionList[i] = Math.random();
     }
-    console.log(heightFunctionList);
 
     for (; i <= num_vertices + num_edges; i++) {
 
@@ -95,6 +113,22 @@ function fillArrays(arr) {
         faces.push(repeteArr);
     }
     console.log(vertices, edges, faces);
+
+
+    // sorting faces wrt coordinates
+    function compare(a, b) {
+        if (vertices[a[0]].z < vertices[b[0]].z) {
+            return -1;
+        }
+        if (vertices[a[0]].z > vertices[b[0]].z) {
+            return 1;
+        }
+        return 0;
+    }
+
+    faces.sort(compare);
+
+    console.log(vertices);
     makePoints();
     makeLink();
 
@@ -160,12 +194,6 @@ function makeLink() {
     console.log(LinkStruct);
 }
 
-function findMaximums(i) {}
-
-function findSaddles(i) {
-
-}
-
 var criticalPoints = () => {
     let criticalPoints = [];
     for (let i = 1; i <= num_vertices; i++) {
@@ -204,6 +232,7 @@ var criticalPoints = () => {
 }
 
 function findCricticalPoints() {
+    let minimaPoints = [];
     let maximaPoints = [];
     let saddlePoints = [];
     for (let i = 1; i <= num_vertices; i++) {
@@ -230,33 +259,44 @@ function findCricticalPoints() {
                 numberOfChangeOfSign++;
             }
             if (heightFunctionList[node] === heigthOfi)
-                numberOfChangeOfSign += 100
+                numberOfChangeOfSign += 0
         }
         if (numberOfChangeOfSign === 0) {
-            console.log("Maxima or Minima", i);
-            console.log(LinkStruct[i], heigthOfi);
-            for (let node of i_link) {
-                console.log(heightFunctionList[node], node);
+            if (currSign === 1) {
+                betti0++;
+                minimaPoints.push(i);
             }
-            maximaPoints.push(i);
+            if (currSign === -1) {
+                betti2++;
+                maximaPoints.push(i);
+            }
+            // console.log("Maxima or Minima", i);
+            // console.log(LinkStruct[i], heigthOfi);
+            // for (let node of i_link) {
+            //     console.log(heightFunctionList[node], node);
+            // }
+
         }
-        if (numberOfChangeOfSign === 3) {
-            console.log("Saddle", i);
-            console.log(LinkStruct[i], heigthOfi);
-            for (let node of i_link) {
-                console.log(heightFunctionList[node], node);
-            }
+        if (numberOfChangeOfSign === 3 || numberOfChangeOfSign === 4) {
+            betti1++;
+            // console.log("Saddle", i);
+            // console.log(LinkStruct[i], heigthOfi);
+            // for (let node of i_link) {
+            //     console.log(heightFunctionList[node], node);
+            // }
             saddlePoints.push(i);
         }
+        if (numberOfChangeOfSign != 0 && numberOfChangeOfSign != 3 && numberOfChangeOfSign != 1 && numberOfChangeOfSign != 2 && numberOfChangeOfSign != 4)
+            console.log("number of sign changes: ", numberOfChangeOfSign);
     }
-    return [maximaPoints, saddlePoints];
+    return [minimaPoints, maximaPoints, saddlePoints];
 }
 
 
 function makePoints() {
     points[0] = null;
     for (let i = 1; i <= num_vertices; i++) {
-        points.push(new THREE.Vector3(vertices[i][0], vertices[i][1], vertices[i][2]));
+        points.push(new THREE.Vector3(vertices[i].x, vertices[i].y, vertices[i].z));
     }
 }
 
@@ -309,7 +349,9 @@ var triangulateKFacesWithShapes = (function() {
             // transform geometry back to the initial coordinate system
             geometry.applyMatrix(_basis);
             // add the face to the object
-            object.add(new THREE.Mesh(geometry, material));
+            var temp_face = new THREE.Mesh(geometry, material);
+            object.add(temp_face);
+            EdgeGeometrys.push(temp_face);
         }
         return object;
     };
@@ -343,16 +385,64 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+function displayCriticalPoints() {
+    // displaying critical points
+    let [minimaPoints, maximaPoints, saddlePoints] = findCricticalPoints();
+    console.log(maximaPoints.length, saddlePoints.length)
+    for (let p of maximaPoints) {
+        const geometry = new THREE.SphereGeometry(markedPointRadii.radius, 32, 32); // (radius, widthSegments, heightSegments)
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.name = `maxPoint${p}`;
+        sphere.position.x = vertices[p].x;
+        sphere.position.y = vertices[p].y;
+        sphere.position.z = vertices[p].z;
+
+        scene.add(sphere);
+    }
+    for (let p of saddlePoints) {
+        const geometry = new THREE.SphereGeometry(markedPointRadii.radius, 32, 32); // (radius, widthSegments, heightSegments)
+        const material = new THREE.MeshBasicMaterial({ color: 0xff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.name = `saddlePoint${p}`
+        sphere.position.x = vertices[p].x;
+        sphere.position.y = vertices[p].y;
+        sphere.position.z = vertices[p].z;
+
+        scene.add(sphere);
+    }
+    for (let p of minimaPoints) {
+        const geometry = new THREE.SphereGeometry(markedPointRadii.radius, 32, 32); // (radius, widthSegments, heightSegments)
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.name = `saddlePoint${p}`
+        sphere.position.x = vertices[p].x;
+        sphere.position.y = vertices[p].y;
+        sphere.position.z = vertices[p].z;
+
+        scene.add(sphere);
+    }
+
+}
+
 function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
     camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.1, 1000.0);
-    camera.position.z = 30;
+    camera.position.z = 80;
     // camera.lookAt(new THREE.Vector3(0, 0, 0));
     renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     controls = new OrbitControls(camera, renderer.domElement);
+
     obj = triangulateKFacesWithShapes(points, faces);
-    scene.add(obj);
+    // scene.add(obj);
+
+    newObj = new THREE.Object3D();
+    for (let f = 0; f < frames; f++) {
+        newObj.add(EdgeGeometrys[f]);
+    }
+    scene.add(newObj);
+
     window.addEventListener("resize", onWindowResize, false);
     onWindowResize();
     document.body.appendChild(renderer.domElement);
@@ -362,28 +452,16 @@ function init() {
     const gridHelper = new THREE.GridHelper(size, divisions);
     scene.add(gridHelper);
 
-    // displaying critical points
-    let [maximaPoints, saddlePoints] = findCricticalPoints();
-    for (let p of maximaPoints) {
-        const geometry = new THREE.SphereGeometry(0.5, 32, 32); // (radius, widthSegments, heightSegments)
-        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-        const sphere = new THREE.Mesh(geometry, material);
-        sphere.position.x = vertices[p][0];
-        sphere.position.y = vertices[p][1];
-        sphere.position.z = vertices[p][2];
+    displayCriticalPoints();
 
-        scene.add(sphere);
-    }
-    for (let p of saddlePoints) {
-        const geometry = new THREE.SphereGeometry(0.5, 32, 32); // (radius, widthSegments, heightSegments)
-        const material = new THREE.MeshBasicMaterial({ color: 0xff00 });
-        const sphere = new THREE.Mesh(geometry, material);
-        sphere.position.x = vertices[p][0];
-        sphere.position.y = vertices[p][1];
-        sphere.position.z = vertices[p][2];
+    // const Controls = gui.addFolder('Controls')
+    // Controls.add(obj.rotation, 'x', 0, Math.PI * 2);
+    // Controls.add(obj.rotation, 'y', 0, Math.PI * 2);
+    // Controls.add(obj.rotation, 'z', 0, Math.PI * 2);
+    // Controls.add(markedPointRadii, 'radius', 0, 10);
+    // Controls.open()
 
-        scene.add(sphere);
-    }
+    console.log("betti0:", betti0, "  betti1:", betti1, "  betti2:", betti2);
 }
 
 function onWindowResize() {
@@ -392,13 +470,33 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+let frames = 0;
+let newObj = new THREE.Object3D();
+let perFrameIncrement = 50;
+
 function animate(time) {
     controls.update();
-    // obj.rotation.x += 0.01;
-    // obj.rotation.y += 0.02;
 
-    // obj = triangulateKFacesWithShapes(points, faces);
-    // scene.add(obj);
+    // if (frames % 1 === 0) {
+    //     if (frames * perFrameIncrement <= num_triangles) {
+    //         scene.remove(newObj);
+    //         newObj = new THREE.Object3D();
+    //         for (let f = 0; f < frames * perFrameIncrement; f++) {
+    //             newObj.add(EdgeGeometrys[f]);
+    //         }
+    //         scene.add(newObj);
+    //     }
+    // }
+
+    scene.remove(newObj);
+    newObj = new THREE.Object3D();
+    for (let f = 0; f < num_triangles; f++) {
+        newObj.add(EdgeGeometrys[f]);
+    }
+    scene.add(newObj);
+
+    if (frames * perFrameIncrement <= num_triangles)
+        frames++;
 
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
